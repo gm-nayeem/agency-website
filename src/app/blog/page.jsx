7820215b16a4fd0f739f0428f1f47fc20feed038
golam mostafa <gmnayeem25@gmail.com>
2024-01-02 01:41:19 +0600

    add deployment url

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,7 @@ import styles from "./blog.module.css";
 
 // FETCH DATA WITH AN API
 const getData = async () => {
-    const res = await fetch("http://localhost:3000/api/blog", { next: { revalidate: 3600 } });
+    const res = await fetch("https://agency-website-app.vercel.app/api/blog", { next: { revalidate: 3600 } });
 
     if (!res.ok) {
         throw new Error("Something went wrong");
